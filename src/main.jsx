52f256397eed854cd093cb8610109676069a84c1
AminulIslamorange <aminulislamorange@gmail.com>
2025-12-21 12:14:50 +0600

    benefits added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import './index.css'
 import { router } from './Router/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <div className='max-w-7xl mx-auto font-urbanist'>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,4 +1,6 @@
 import Banner from "./Banner/Banner";
+import Benefits from "./Benefit/Benefits";
+import ClientLogo from "./ClientLogo/ClientLogo";
 import HowItsWork from "./HowItsWork/HowItsWork";
 import Services from "./Services/Services";
 
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner></Banner>
             <HowItsWork></HowItsWork>
             <Services></Services>
+            <ClientLogo></ClientLogo>
+            <Benefits></Benefits>
         </div>
     );
 };
