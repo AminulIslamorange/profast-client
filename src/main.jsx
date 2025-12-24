4f7f9f2b23edcee45930a4daeb5f99e8863d76c1
AminulIslamorange <aminulislamorange@gmail.com>
2025-12-24 12:35:57 +0600

    my parcel started

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,20 @@ import { RouterProvider } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import AuthProvider from './context/AuthContext/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 AOS.init();
 
-
+const queryClient = new QueryClient()
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <div className='max-w-7xl mx-auto font-urbanist'>
-    <AuthProvider> 
+    <QueryClientProvider client={queryClient}>
+       <AuthProvider> 
       <RouterProvider router={router} />
       </AuthProvider>
+    </QueryClientProvider>
   </div>
   </StrictMode>,
 )
